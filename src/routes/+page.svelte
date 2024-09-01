ba69a0006cc7e7650e530bd4e24a209aec31c846
Victor Voronov <victor@voronov.io>
2024-08-31 22:58:42 -0400

    nvm, lemme steal css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,9 @@
     ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
     ['.', '.', '.', '.', '8', '.', '.', '7', '9']
   ];
+
   const keys = Array.from({ length: 9 }).map((_, i) => i + 1);
+  let isKeysHidden = $state(true);
 </script>
 
 <h1>Welcome to Sudoku (proper)</h1>
@@ -26,6 +28,7 @@
               return;
             }
             console.log(cell);
+            isKeysHidden = false;
 
             // display number menu
           }}>{cell}</button
@@ -36,7 +39,8 @@
   </div>
   <br />
   <br />
-  <div id="keys">
+
+  <div id="keys" class:visually-hidden={isKeysHidden}>
     {#each keys as key}
       <button onclick={() => console.log(key)}>
         {key}
@@ -46,4 +50,111 @@
 </div>
 
 <style>
+  :root {
+    --font-body: Arial, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+      Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    /*--font-mono: 'Fira Mono', monospace;*/
+    --font-mono: monospace;
+    --color-bg-0: rgb(202, 216, 228);
+    --color-bg-1: hsl(209, 36%, 86%);
+    --color-bg-2: hsl(224, 44%, 95%);
+    --color-theme-1: #ff3e00;
+    --color-theme-2: #4075a6;
+    --color-text: rgba(0, 0, 0, 0.7);
+    --column-width: 42rem;
+    --column-margin-top: 4rem;
+    /*font-family: var(--font-body);*/
+    font-family: var(--font-mono);
+    color: var(--color-text);
+  }
+
+  body {
+    min-height: 100vh;
+    margin: 0;
+    background-attachment: fixed;
+    background-color: var(--color-bg-1);
+    background-size: 100vw 100vh;
+    background-image: radial-gradient(
+        50% 50% at 50% 50%,
+        rgba(255, 255, 255, 0.75) 0%,
+        rgba(255, 255, 255, 0) 100%
+      ),
+      linear-gradient(180deg, var(--color-bg-0) 0%, var(--color-bg-1) 15%, var(--color-bg-2) 50%);
+  }
+
+  h1,
+  h2,
+  p {
+    font-weight: 400;
+  }
+
+  p {
+    line-height: 1.5;
+  }
+
+  a {
+    color: var(--color-theme-1);
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  h1 {
+    font-size: 2rem;
+    text-align: center;
+  }
+
+  h2 {
+    font-size: 1rem;
+  }
+
+  pre {
+    font-size: 16px;
+    font-family: var(--font-mono);
+    background-color: rgba(255, 255, 255, 0.45);
+    border-radius: 3px;
+    box-shadow: 2px 2px 6px rgb(255 255 255 / 25%);
+    padding: 0.5em;
+    overflow-x: auto;
+    color: var(--color-text);
+  }
+
+  .text-column {
+    display: flex;
+    max-width: 48rem;
+    flex: 0.6;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+  }
+
+  input,
+  button {
+    font-size: inherit;
+    font-family: inherit;
+  }
+
+  button:focus:not(:focus-visible) {
+    outline: none;
+  }
+
+  @media (min-width: 720px) {
+    h1 {
+      font-size: 2.4rem;
+    }
+  }
+
+  .visually-hidden {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: auto;
+    margin: 0;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+    white-space: nowrap;
+  }
 </style>
