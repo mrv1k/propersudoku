7be0d5f0ac1752aeaba33c21aa80e6816a06959f
Victor Voronov <victor@voronov.io>
2024-09-20 20:49:32 -0400

    working dfs solution
    (stolen)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,55 +87,32 @@
 
   const solveInit = (board) => {
     const clone = cloneArray(board);
-    solve(clone);
-    console.log(clone);
-
-    //userBoard = clone;
-    //const clone2 = cloneArray(board);
-    //dfs(clone2, clone2.length);
-    //console.log(clone2);
+    dfs(clone);
   };
-  const solve = (board) => {
+
+  function dfs(board) {
+    // for every cell in the sudoku
     for (let row = 0; row < 9; row++) {
       for (let col = 0; col < 9; col++) {
-        if (board[row][col] === X) {
-          for (let n = 1; n <= 9; n++) {
-            console.count('solve');
-            if (checkAll(row, col, n, board)) {
-              board[row][col] = String(n);
-              if (solve(board)) {
-                return true;
-              }
-              board[row][col] = X;
-            }
-            return false; // no number worked
-          }
-        }
-      }
-    }
-    return true;
-  };
-
-  function dfs(board, n) {
-    // for every cell in the sudoku
-    for (let row = 0; row < n; row++) {
-      for (let col = 0; col < n; col++) {
         // if its empty
-        if (board[row][col] !== '.') continue;
+        if (board[row][col] !== X) {
+          continue;
+        }
         // try every number 1-9
         for (let i = 1; i <= 9; i++) {
-          console.count('dfs');
           const c = i.toString();
           // if that number is valid
-          if (isValid(board, row, col, n, c)) {
+          if (isValid(board, row, col, c)) {
             board[row][col] = c;
             // continue search for that board, ret true if solution is reached
-            if (dfs(board, n)) return true;
+            if (dfs(board)) {
+              return true;
+            }
           }
         }
         // solution wasnt found for any num 1-9 here, must be a dead end...
         // set the current cell back to empty
-        board[row][col] = '.';
+        board[row][col] = X;
         // ret false to signal dead end
         return false;
       }
@@ -144,10 +121,10 @@
     return true;
   }
 
-  function isValid(board, row, col, n, c) {
+  function isValid(board, row, col, c) {
     const blockRow = Math.floor(row / 3) * 3;
     const blockCol = Math.floor(col / 3) * 3;
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < 9; i++) {
       if (board[row][i] === c || board[i][col] === c) return false;
       const curRow = blockRow + Math.floor(i / 3);
       const curCol = blockCol + Math.floor(i % 3);
